a041a6d45ffe0fbfa9d89ceff5260dc787120335
agee1 <orangemadcow@yahoo.com>
2020-07-21 18:00:44 -0700

    Updates

---
+++ precache-manifest.203615263f6103376ca0ad38c9b0c902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ac7af711e5d397c4bc4bac4a828cef",
+    "url": "/mj-scoreboard/index.html"
+  },
+  {
+    "revision": "1a14a7a19bec88e8d126",
+    "url": "/mj-scoreboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6948aa7045620d4887d0",
+    "url": "/mj-scoreboard/static/css/main.b19612fe.chunk.css"
+  },
+  {
+    "revision": "1a14a7a19bec88e8d126",
+    "url": "/mj-scoreboard/static/js/2.cda0769c.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/mj-scoreboard/static/js/2.cda0769c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6948aa7045620d4887d0",
+    "url": "/mj-scoreboard/static/js/main.936acb04.chunk.js"
+  },
+  {
+    "revision": "fa0ed6ad63d3d59f362b",
+    "url": "/mj-scoreboard/static/js/runtime-main.613abce5.js"
+  }
+]);
